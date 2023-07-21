d7c2b4366b2bcdc5a6b5936916acf6aea32651a9
JefferssonVMT <jefferssonmanzanarez@gmail.com>
2023-07-21 02:45:19 -0600

    Implementado Notificaciones con Firebase

--- CitasAppXamarin/ActivityLogin.cs
+++ CitasAppXamarin/ActivityLogin.cs
@@ -5,6 +5,10 @@ using Android.Runtime;
 using Android.Widget;
 using AndroidX.AppCompat.App;
 using System;
+using Android.Gms.Common;
+using Firebase.Messaging;
+using Firebase.Iid;
+using Android.Util;
 
 namespace CitasAppXamarin
 {
@@ -13,12 +17,25 @@ namespace CitasAppXamarin
     {
         string Email, Contra;
 
+        static readonly string TAG = "MainActivity";
+
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+
+        [Obsolete]
+#pragma warning disable CS0809 // El miembro obsoleto invalida un miembro no obsoleto
         protected override void OnCreate(Bundle savedInstanceState)
+#pragma warning restore CS0809 // El miembro obsoleto invalida un miembro no obsoleto
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.login);
+
+            IsPlayServicesAvailable();
+
+            CreateNotificationChannel();
+            Log.Debug(TAG, "InstanceID token: " + FirebaseInstanceId.Instance.Token);
 
             FindViewById<Button>(Resource.Id.button1).Click += ActivityLogin_Click;
             FindViewById<TextView>(Resource.Id.textView_Registro).Click += ActivityLogin_Click1;
@@ -56,6 +73,39 @@ namespace CitasAppXamarin
             {
                 Toast.MakeText(this, "Usuario inexistente o datos erroneos", ToastLength.Long).Show();
             }
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                Finish();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID, "FCM Notifications", NotificationImportance.Default)
+            {
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
         }
     }
 }
---
+++ CitasAppXamarin/MyFirebaseIIDService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Android.App;
+using Firebase.Iid;
+using Android.Util;
+
+namespace FCMClient
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    [Obsolete]
+    public class MyFirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIIDService";
+
+        [Obsolete]
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+        }
+    }
+}
